8ea37aa48ce7c5ee8c54a72a87021a7b92cd1af4
giannamar4 <90292508+giannamar4@users.noreply.github.com>
2021-11-09 15:10:21 -0700

    fixed version

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-let turn = 1;
-let win = -1;
-let clickCounter = 0;
-let matrix = [
-  [-1,-1,-1],
-  [-1,-1,-1],
-  [-1,-1,-1]
+let cells = document.querySelectorAll('.cell')
+cells = Array.from(cells)
+let currentPlayer = "X"
+let win = 0
+let clickCount = 0
+let winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ]
 
-function playerAction(elem, row, col){
-  if(elem.textContent != "") return;
-
-if(win != -1) return;
-
-clickCounter++
-
-  matrix[row][col] = turn;
-if(turn ==1){
-    elem.textContent = 'X';
-    document.getElementById('turnNum').textContent = "Player 2 (O)"
-    turn = 2
-
-}
-else if(turn == 2){
-    elem.textContent = 'O'
-    document.getElementById('turnNum').textContent = "Player 1 (X)"
-    turn = 1;
-}
-for(let i =0; i<3; i++){
-  // row check
-  if(matrix[i][0] == matrix[i][1] == matrix[i][2]) win = matrix[i][0]
- /* if(matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2]) win = matrix[i][0]
-  if(matrix[0][0] == matrix[0][1] == matrix[0][2]) win = matrix[i][0]*/
-  // col check
-  if(matrix[0][i] == matrix[1][i] == matrix [2][i]) win = matrix[0][i]
- 
-}
- // diagonal check
- if(matrix[0][0] == matrix[1][1] ==matrix[2][2]) win = matrix[1][1]
- if(matrix[0][2] == matrix[1][1] ==matrix[2][0]) win = matrix[1][1]
-
- if(win != -1){
-  document.getElementById('turnNum').textContent = "Player " + win + " has won!"
- }
- 
- if(clickCounter == 9 && win == -1){
-  document.getElementById('turnNum').textContent = "It's a draw!"
- }
+function checkForWinner(){
+    winningCombinations.forEach(function(combination){
+        let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer)
+        if(check){
+            highlightCells(combination)
+        }
+    })
 }
 
+function highlightCells(combination){
+    combination.forEach(function(idx){
+        cells[idx].classList.add("highlight")
+        document.getElementById('turnNum').textContent = currentPlayer + ' has won!'
+        win++
+    })
+}
+
+cells.forEach(function(cell){
+    cell.addEventListener('click', function(){
+        if(cell.innerText.trim() != "") return
+        cell.innerText = currentPlayer
+        if(currentPlayer == 'X'){
+          document.getElementById('turnNum').textContent = 'Current Turn: O'
+          clickCount++
+        }
+        else{
+          document.getElementById('turnNum').textContent = 'Current Turn: X'
+          clickCount++
+        }        
+        checkForWinner()
+        currentPlayer = currentPlayer == "X" ? "O" : "X"
+        
+    })
+})
+
+function reset(){
+  location.reload()
+}
+if(win != 1 && clickCount == 9){
+  document.getElementById('turnNum').textContent = "It's a Draw"
+}
