b4bfae054732760f9c74a726bc46d73fa8ea1d5c
giannamar4 <90292508+giannamar4@users.noreply.github.com>
2021-11-08 18:04:09 -0700

    board finished
    finished and stylized tic tac toe board. beginning java script steps for player turns

---
+++ index.js
@@ -0,0 +1,11 @@
+let player = 0
+function mark(){
+    if(player = 0){
+  document.getElementsByIdClassName("box").innerHTML = "X";
+  player++}
+    if(player = 1){
+        document.getElementsByClassName("box").textContent = "O";
+        player--
+        }
+}
+
